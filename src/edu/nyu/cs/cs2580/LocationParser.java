46d6023cd2ed775da1dd5051c0df265296292de8
bsanaya <sab894@nyu.edu>
2016-12-07 20:14:17 -0500

    Update LocationParser.java
    added query-segmentation. now working on filtering for obtaining candidate locations.

--- src/edu/nyu/cs/cs2580/LocationParser.java
+++ src/edu/nyu/cs/cs2580/LocationParser.java
@@ -1,5 +1,7 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.*;
+
 /**
  * Created by stephen on 12/3/16.
  */
@@ -7,6 +9,9 @@ public class LocationParser {
 
     private SpatialEntityKnowledgeBase _gkb;
     private Indexer _indexer;
+    private String[] tokens;
+    private int length;
+    private List<String> final_location_candidates = new ArrayList<>();
 
     public LocationParser(Indexer indexer, SpatialEntityKnowledgeBase gkb) {
         _indexer = indexer;
@@ -18,6 +23,50 @@ public class LocationParser {
         QueryBoolGeo toReturn = new QueryBoolGeo(inputString, null, null);
         return toReturn;
     }
+
+    public void SegmentThisQueryString(String inputString) {
+            length = inputString.length();
+            String[] tokens = inputString.split("\\s");
+            int startIndex, endIndex;
+            int nextSegmentIndex=0;
+            String candidateOfCandidate="";
+            for(int i=nextSegmentIndex; i<length; i++){
+
+                for(int j=i;j<length; j++){
+                    startIndex = i;
+                    endIndex = j;
+                    if(startIndex == endIndex){
+                        candidateOfCandidate = tokens[startIndex];
+                    }
+                    else{
+                        for(int k=startIndex; k<endIndex; k++){
+                            candidateOfCandidate += tokens[k] + " ";
+                        }
+                        candidateOfCandidate += tokens[endIndex];
+                    }
+
+                    if(testCandidatePossibility(candidateOfCandidate)) {
+                        final_location_candidates.add(candidateOfCandidate);
+                        if(j != (length - 1)){
+                            nextSegmentIndex = j+1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+
+    }
+
+    public boolean testCandidatePossibility(String candidateOfCandidate){
+
+        //here we look up to the geo-spatial data for whether the input string exists as one of the location entity
+        return false;
+
+    }
+
+
 
     //===============================================
     // INCLUDE METHOD FOR STATISTICAL SEGMENTATION
