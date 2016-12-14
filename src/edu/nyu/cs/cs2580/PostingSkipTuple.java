546941aa18fd65ae44487b37edbb03725ac851f8
PatrickYuen <ply209@nyu.edu>
2016-12-14 01:22:25 -0500

    test

---
+++ src/edu/nyu/cs/cs2580/PostingSkipTuple.java
@@ -0,0 +1,17 @@
+package edu.nyu.cs.cs2580;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PostingSkipTuple implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	public Integer[] postingList;
+	public Integer[] skipList;
+	
+	public PostingSkipTuple(List<Integer> postingList, List<Integer> skipList) {
+		this.postingList = postingList.toArray(new Integer[postingList.size()]);
+		this.skipList = skipList.toArray(new Integer[skipList.size()]);
+	}
+}
